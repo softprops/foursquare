833d31b5d247df0886188746aabe4df3ff1415cd
softprops <d.tangren@gmail.com>
2017-12-15 01:37:43 -0500

    update example

--- examples/recommendations.rs
+++ examples/recommendations.rs
@@ -28,8 +28,12 @@ fn run() -> Result<()> {
             match core.run(
                 foursq.venues().recommendations(
                     &RecommendationsOptions::builder()
-                        .ll("37.5665,126.9780")
-                        .features(vec![Feature::Wifi, Feature::NotChain])
+                        .ll("40.7686834,-73.9539324")
+                        .features(vec![
+                            Feature::TakesCreditCards,
+                            Feature::Wifi,
+                            Feature::DogFriendly,
+                        ])
                         .build()?,
                 ),
             ) {
